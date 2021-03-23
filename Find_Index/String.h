fcd18c2fa3fc7ed4504b2b3da518ac1fa01e9772
oris <oris@mta.ac.il>
2021-03-23 04:14:20 +0200

    first sort

---
+++ Find_Index/String.h
@@ -0,0 +1,96 @@
+#ifndef STR_H_
+#define STR_H_
+#include <iostream>
+using namespace std;
+
+const int MIN_SIZE = 2;
+const int MAX_SIZE = 7;
+extern void errorMsg();
+
+class String
+{
+	char* str;
+	int logSize;
+	int phisicalSize;
+
+public:
+	String() :str(new char[MIN_SIZE]), phisicalSize(MIN_SIZE), logSize(0) {}
+
+	~String()
+	{
+		delete str;
+	}
+
+	void resize()
+	{
+		char *newStr = new char[phisicalSize *= 2];
+		copy(newStr);
+		delete str;
+		str = newStr;
+	}
+
+	void compress()
+	{
+		char *newStr = new char[logSize + 1];
+		copy(newStr);
+		delete str;
+		str = newStr;
+		phisicalSize = logSize;
+	}
+
+	operator char*() { return str; }
+
+	friend ostream& operator<<(ostream& os, const String& str)
+	{
+		int i = 0;
+		while (i < str.logSize)
+		{
+			os << str.str[i];
+			i++;
+		}
+		return os;
+	}
+	void copy(char* s)
+	{
+		int i;
+		for (i = 0; i < logSize + 1; i++)
+			s[i] = str[i];
+	}
+
+	void getNum()
+	{
+		int count = 0;
+		char ch = '0';
+		bool isNum = ch >= '0' || ch <= '9';
+		bool isSecIter = count == 1;
+		bool isAfterPoint = false;
+
+		
+		cin >> ch;
+
+		while (ch != '\n')
+		{
+			if (logSize >= phisicalSize-1)
+				resize();
+			//vlidation chack
+			if (!isNum && ch != '\n')
+				errorMsg();
+			
+			str[logSize] = ch;
+			logSize++;
+			if (isAfterPoint)
+				count++;
+			cin.get(ch);
+			isAfterPoint = ch == '.';
+		}
+		//vlidation chack
+		if (isAfterPoint && (count > 3 || count < 1)|| ch == '\n' && count == 0)
+			errorMsg();
+		compress();
+		str[logSize] = '\0';
+	}
+
+	const int& getSize() { return logSize;}
+};
+
+#endif // !STR
--- Find_Index/main.cpp
+++ Find_Index/main.cpp
@@ -1,35 +1,73 @@
 #include <iostream>
+#include "String.h"
 using namespace std;
 
+void errorMsg()
+{
+	cout << "wrong input" << endl;
+	exit(1);
+}
+void swap(double& i, double& j)
+{
+	double temp = i;
+	i = j;
+	j = temp;
+}
+
+void insertionSort(double *arr, int size)
+{
+	int i = 0;
+	int j = 0;
+	for (i; i < size; i++)
+	{
+		j = i;
+		while (j > 0 && arr[j - 1] > arr[j])
+		{
+			swap(arr[j], arr[j - 1]);
+			j--;
+		}
+
+	}
+}
 
 
 int main()
 {
-	int n, i, j, counter, index;
+	int n, i, j, counter, idx;
+	double temp;
+	char ch;
 	cout << "Please enter the number of numbers you would like to enter and after that," <<
 		" enter the index you would like to find" << endl;
 	cin >> n >> i;
 
-	cout << "Please enter the number" << endl;
-
-
-	char ch = 0;
-
-	counter = index = 0;
-	while (ch != 'e')
-	{
-		
-		counter++;
-		cout << "Press 'e' to stop" << endl;
-	}
-
-	if (counter < n)
-	{
-		cout << "wrong input" << endl;
-		exit(1);
-	}
-
+	cout << "Please enter the numbers from which you would like to find the index" << endl;
 	
+
+	double *arr = new double[n];
+	
+	counter = idx = 0;
+	for(idx = 0; idx < n; idx++)
+	{
+		String str;
+		str.getNum();
+		arr[idx] = atof(str);
+	}
+	cout << endl;
+
+	for (idx = 0; idx < n; idx++)
+	{
+		cout << arr[idx] << endl;
+	}
+	cout << endl;
+	insertionSort(arr, n);
+
+	for (idx = 0; idx < n; idx++)
+	{
+		cout << arr[idx] << endl;
+	}
+	cout << endl;
+
+	cout << arr[i-1];
 
 	return 0;
 }
