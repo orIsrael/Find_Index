1599622c48f194f1f69e4beeea6c0ad0223a783f
nitsan <nitsanab@mta.ac.il>
2021-03-29 00:04:13 +0300

    time

--- Find_Index/main.cpp
+++ Find_Index/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include<chrono>
+#include<fstream>
+#include<iomanip>
 #include "String.h"
 using namespace std;
 
@@ -99,6 +102,7 @@ int main()
 	int n, i, j, counter, idx;
 	double temp;
 	char ch;
+
 	cout << "Please enter the number of numbers you would like to enter and after that," <<
 		" enter the index you would like to find" << endl;
 	cin >> n >> i;
@@ -117,12 +121,33 @@ int main()
 	}
 	cout << endl;
 
+	double* CopyArr = new double[n];
+	for (idx = 0; idx < n; idx++)
+	{
+		CopyArr[idx] = arr[idx];
+	}
+
 	for (idx = 0; idx < n; idx++)
 	{
 		cout << arr[idx] << endl;
 	}
 	cout << endl;
-	insertionSort(arr, n);
+	
+	auto start = chrono::high_resolution_clock::now();
+	// unsync the I/O of C and C++.
+	ios_base::sync_with_stdio(false);
+	insertionSort(arr, n);// Here you put the name of the function you wish to measure
+	auto end = chrono::high_resolution_clock::now();
+
+	// Calculating total time taken by the program.
+	double time_taken =
+		chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+	time_taken *= 1e-9;
+	ofstream myfile("Measure.txt", ios::app); // The name of the file
+	myfile << "Time taken by function <name-of-fun> is : " << fixed
+		<< time_taken << setprecision(9);
+	myfile << " sec" << endl;
+	myfile.close();
 
 	for (idx = 0; idx < n; idx++)
 	{
@@ -134,27 +159,24 @@ int main()
 
 	
 //	cout << selection(arr,n, i) << endl;
+	cout << select(CopyArr, 0, n, i) << endl;
 
-	cout << select(arr, 0, n, i) << endl;
-
-	/////זמני ריצה
-	//auto start = chrono::high_resolution_clock::now();
-	//// unsync the I/O of C and C++.
-	//ios_base::sync_with_stdio(false);
-	//fun();// Here you put the name of the function you wish to measure
-	//auto end = chrono::high_resolution_clock::now();
-	//3
-	//	// Calculating total time taken by the program.
-	//	double time_taken =
-	//	chrono::duration_cast<chrono::nanoseconds>(end - start).count();
-	//time_taken *= 1e-9;
-	//ofstream myfile("Measure.txt"); // The name of the file
-	//myfile << "Time taken by function <name-of-fun> is : " << fixed
-	//	<< time_taken << setprecision(9);
-	//myfile << " sec" << endl;
-	//myfile.close();
-
-
+	auto start2 = chrono::high_resolution_clock::now();
+	// unsync the I/O of C and C++.
+	ios_base::sync_with_stdio(false);
+// Here you put the name of the function you wish to measure
+	select(arr, 0, n, i);
+	auto end2 = chrono::high_resolution_clock::now();
+	
+		// Calculating total time taken by the program.
+		double time_taken2 =
+		chrono::duration_cast<chrono::nanoseconds>(end2 - start2).count();
+	time_taken2 *= 1e-9;
+	ofstream myfile2("Measure.txt",ios::app); // The name of the file
+	myfile2 << "Time taken by function <name-of-fun> is : " << fixed
+		<< time_taken2 << setprecision(9);
+	myfile2 << " sec" << endl;
+	myfile2.close();
 
 	return 0;
 }
